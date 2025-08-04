dbcd104ce7595c0c94cf433a1d8285488ccfd74a
Rudr <rudrprasad@yahoo.com>
2025-08-05 11:40:05 +1200

    week 3 cs324

---
+++ week 3/presentation_layer/UI.java
@@ -0,0 +1,78 @@
+package presentation_layer;
+
+import java.util.*;
+import business_layer.*;
+
+class UI{
+	private BLcustomer cus;
+
+	public UI(){
+		cus = new BLcustomer();
+	}
+
+	private int choice;
+
+	public int getChoice() {
+		return choice;
+	}
+
+	public void setChoice(int choice) {
+		this.choice = choice;
+	}
+
+	public void add_customer(){
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter id: ");
+		cus.setCusId(in.next());
+
+		System.out.println("Enter last name: ");
+		cus.setLName(in.next());
+		System.out.println("Enter first name: ");
+		cus.setFName(in.next());
+
+		try{
+			cus.add();
+			System.out.println("addition successful");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public void delete_customer(){
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Enter id: ");
+		cus.setCusId(in.next());
+
+		try{
+			cus.delete();
+			System.out.println("addition successful");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public void print(){
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("press 1 to add a customer");
+		System.out.println("press 2 to delete a customer");
+		System.out.println("press 3 to exit an application");
+		choice = in.nextInt();
+
+		if (choice == 1){
+			this.add_customer();
+		}
+		else if (choice == 2){
+			this.delete_customer();
+		}
+		else if (choice ==3){
+			System.exit(1);
+		}
+		else{
+			System.exit(1);
+		}
+	}
+}
